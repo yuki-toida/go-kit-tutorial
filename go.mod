6b8b9791652493e12deff461bdb36da374c6728a
YukiToida <yuki.toidax@gmail.com>
2019-02-27 10:07:56 +0900

    go mod

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/yuki-toida/go-kit-tutorial
+
+go 1.12
+
+require (
+	github.com/go-kit/kit v0.8.0
+	github.com/go-logfmt/logfmt v0.4.0
+	github.com/kr/logfmt v0.0.0-20140226030751-b84e30acd515
+	github.com/prometheus/client_golang v0.9.2
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+// StringService provides operations on strings.
+type StringService interface {
+	Uppercase(string) (string, error)
+}
+
+// stringService is a concrete implementation of StringService
+type stringService struct{}
+
+func (stringService) Uppercase(s string) (string, error) {
+	if s == "" {
+		return "", errors.New("empty string")
+	}
+	return strings.ToUpper(s), nil
+}
+
+// For each method, we define request and response structs
+type uppercaseRequest struct {
+	S string `json:"s"`
+}
+
+// Endpoints are a primary abstraction in go-kit. An endpoint represents a single RPC (method in our service interface)
+func makeUppercaseEndpoint(svc StringService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(uppercaseRequest)
+		v, err := svc.Uppercase(req.S)
+
+		res := &struct {
+			V   string `json:"s"`
+			Err string `json:"err"`
+		}{V: v}
+
+		if err != nil {
+			res.Err = err.Error()
+			return res, nil
+		}
+		return res, nil
+	}
+}
+
+// Transports expose the service to the network. In this first example we utilize JSON over HTTP.
+func main() {
+	svc := stringService{}
+
+	uppercaseHandler := httptransport.NewServer(
+		makeUppercaseEndpoint(svc),
+		decodeUppercaseRequest,
+		encodeResponse,
+	)
+
+	http.Handle("/uppercase", uppercaseHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func decodeUppercaseRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request uppercaseRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
